4fc4a0c0aae5e7889144ccaa70a3ad6ec150a659
Vasyl-007 <vasyl.manyo@gmail.com>
2019-12-17 13:18:59 +0200

    homework_js_morule_06

--- javascript/js-module-06/js/scriptTask_02.js
+++ javascript/js-module-06/js/scriptTask_02.js
@@ -2,5 +2,5 @@
 import users from "./scriptLibrary.js";
 
 const getUsersWithEyeColor = (array, color) =>
-  users.filter(user => user.eyeColor === "blue");
+  users.filter(user => user.eyeColor === color);
 console.log(getUsersWithEyeColor(users, "blue")); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
