deb4367da5a279debca90cf2516dba0a03ffb353
Vasyl-007 <vasyl.manyo@gmail.com>
2020-03-13 20:55:02 +0200

    5-collections

--- trial/practice-5-collections/src/components/taskEditor/TaskEditor.js
+++ trial/practice-5-collections/src/components/taskEditor/TaskEditor.js
@@ -3,7 +3,17 @@ import PropTypes from "prop-types";
 import styles from "./TaskEditor.module.css";
 
 export default class TaskEditor extends Component {
-  state = {};
+  state = {
+    text: "",
+    priority: "normal"
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
   render() {
     return (
       <form className={styles.form}>
@@ -13,7 +23,14 @@ export default class TaskEditor extends Component {
           name="text"
           placeholder="Enter task content..."
         />
-        
+        <label className={styles.label}>
+          Select task priority:
+          <select name="priority" value={priority}>
+            <option value="low">Low</option>
+            <option value="normal">Normal</option>
+            <option value="high">High</option>
+          </select>
+        </label>
       </form>
     );
   }
