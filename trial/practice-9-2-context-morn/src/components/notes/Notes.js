7ffa558a1b54c8819926ef9a203d3e51eb494ce1
Vasyl-007 <vasyl.manyo@gmail.com>
2020-03-19 12:52:57 +0200

    practice context part 3 19.03

--- trial/practice-9-2-context-morn/src/components/notes/Notes.js
+++ trial/practice-9-2-context-morn/src/components/notes/Notes.js
@@ -1,6 +1,7 @@
 import React, { Component, createContext } from "react";
 import Form from "../form/Form";
 import List from "../list/List";
+import { DefaultContext } from "../../App";
 
 export const NotesContext = createContext();
 
@@ -16,23 +17,29 @@ class Notes extends Component {
     }));
     // принимает note и пушит в массив notes
   };
-  
+
   render() {
     console.log("re-re-nder");
     const { notes } = this.state;
     return (
-      <>
-        <NotesContext.Provider
-          value={{
-            theme: "dark",
-            notes
-          }}
-        >
-
-          <Form onGetNoteInfo={this.getNoteInfo} />
-          <List />
-        </NotesContext.Provider>
-      </>
+      <DefaultContext.Consumer>
+        {defContext => {
+          console.log("defContext", defContext);
+          return (
+            <NotesContext.Provider
+              value={{
+                // theme: "dark",
+                notes,
+                handleSubmit: this.getNoteInfo,
+                theme: defContext.theme
+              }}
+            >
+              <Form onGetNoteInfo={this.getNoteInfo} />
+              <List />
+            </NotesContext.Provider>
+          );
+        }}
+      </DefaultContext.Consumer>
     );
   }
 }
