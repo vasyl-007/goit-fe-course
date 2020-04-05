383eff4a33f949cff29cd1bf74001b32aeee8149
Vasyl-007 <vasyl.manyo@gmail.com>
2020-04-06 02:42:38 +0300

    hooks added

--- temp/wb-10-animated-redux/src/HookEffects.js
+++ temp/wb-10-animated-redux/src/HookEffects.js
@@ -16,6 +16,8 @@ const HookEffects = () => {
         <button onClick={() => setVisible(false)}>hide</button>
         <ClassCounter value={value} />
         <HookCounter value={value} />
+        <br />
+        <Notification />
       </div>
     );
   } else {
@@ -40,9 +42,34 @@ class ClassCounter extends Component {
 
 const HookCounter = ({ value }) => {
   useEffect(() => {
-    console.log("useEffect");
+    console.log("mount");
+  }, []);
+  useEffect(() => {
+    console.log("update");
   });
+  useEffect(
+    () => () => {
+      console.log("unmount");
+    },
+    []
+  );
+  useEffect(() => {
+    console.log("useEffect");
+  }, [value]);
   return <p>{value}</p>;
+};
+
+const Notification = () => {
+  const [vis, setVis] = useState(true);
+  useEffect(() => {
+    const timeout = setTimeout(() => setVis(false), 1500);
+    return () => clearTimeout(timeout);
+  });
+  return (
+    <div style={{ backgroundColor: "aqua" }}>
+      {vis && <p>Hello! This message will disappear via 1.5 sec</p>}
+    </div>
+  );
 };
 
 export default HookEffects;
